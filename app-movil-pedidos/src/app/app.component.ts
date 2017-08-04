a2724f5219756a975227c86f653248ab7b71f3a9
AngeloDAvid <angelodavid95@hotmail.es>
2017-08-03 23:00:51 -0500

    Funcionalidad del menu

--- app-movil-pedidos/src/app/app.component.ts
+++ app-movil-pedidos/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform,MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import {MenuPage,MenuTabPage} from '../pages/index.pages';
+import {MenuTabPage,PerfilPage,PedidoActualPage} from '../pages/index.pages';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = MenuTabPage;
+  menu:any =MenuTabPage;
+  perfil:any=PerfilPage;
+  Pedido:any=PedidoActualPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private menucrl:MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,5 +22,10 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  irpagina(page: any){
+    this.rootPage=page;
+    this.menucrl.close();
+  }
+
 }
 
