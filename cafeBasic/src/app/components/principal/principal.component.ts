44a6401bf41fe0e1af485453b197b363c0a0ea8c
TatyMaybeth <tattocsb_00994@hotmail.com>
2017-08-17 08:54:52 -0500

    Principal

--- cafeBasic/src/app/components/principal/principal.component.ts
+++ cafeBasic/src/app/components/principal/principal.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./principal.component.css', './devices.css']
 })
 export class PrincipalComponent implements OnInit {
 
